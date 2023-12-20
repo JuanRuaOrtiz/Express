bc25a6ca0a2d0723540f5b64f02a89ee4536cb42
jesus ramirez <alexanderjunior2017@gmail.com>
2023-12-20 00:18:46 -0500

    project3

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const listViewRouter = require('./list-view-router');
-const listEditRouter = require('./list-edit-router');
+const port = 9000;
+
 
 const task = [
     {
         id: '123456',
         isCompleted: false,
-        descripcion: "walk the dog"
+        descripcion: "hacer aseo"
     },
 ]
 app.get('/task', (req, res) => {
     res.json(task);
   });
 
-app.use('/list-view', listViewRouter);
-app.use('/list-edit',listEditRouter);
-
+  
 app.listen(port,()=>
 {
     console.log('Servidor Express escuchando en puerto: ', port);
